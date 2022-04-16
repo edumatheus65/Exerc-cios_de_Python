bfd1e79124b5ae37cdff7925b487fcaf40d49cdb
edumatheus65 <ematheusneves13@gmail.com>
2022-04-16 15:40:23 -0300

    measurement conversion exercise solved

---
+++ Conversor_de_medidas.py
@@ -0,0 +1,4 @@
+medida = float(input('Distancia em metros: '))
+print(f'A medida de {medida}metros corresponde a \n{medida * 1000:.0f}mm \n{medida*100:.0f}cm \n{medida*10:.0f}dm \n{medida/10:.1f}dam \n{medida/100:.2f} \n{medida/1000:.3f}km.')
+
+
