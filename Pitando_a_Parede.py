dc37c03f56629bde97601a4b954f63415176850f
edumatheus65 <ematheusneves13@gmail.com>
2022-04-18 12:26:53 -0300

    Created wall painting exercise

---
+++ Pitando_a_Parede.py
@@ -0,0 +1,3 @@
+largura = float(input('Informe a largura da parede: '))
+altura = float(input('Informe o altura da parede: '))
+print(f'Sua parede tem a dimensão de {largura}x{altura} e sua área é de {largura * altura}m².')
--- conversor_de_moedas.py
+++ conversor_de_moedas.py
@@ -1,2 +1,3 @@
 moeda = float(input('Quanto dinheiro você tem na carteira? '))
 print(f'Com R${moeda} você pode comprar U$${moeda/4.70:.2f}')
+
