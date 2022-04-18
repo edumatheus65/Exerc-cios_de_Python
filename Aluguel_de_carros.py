6ddba2c33b1455c9379bda1809159ff4fff3fccc
edumatheus65 <ematheusneves13@gmail.com>
2022-04-18 17:30:49 -0300

    Done exercise car rental

---
+++ Aluguel_de_carros.py
@@ -0,0 +1,4 @@
+qtd_km = float(input('Quantos km foi rodado com o carro? '))*0.15
+qtd_Dias = int(input('Quantos dias o carro foi usado? '))*60
+print(f'Então será cobrado R${qtd_km + qtd_Dias:.2f}reais')
+
