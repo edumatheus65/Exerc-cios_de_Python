21dfc08881fd71059c2473e52dc77db14b5639d0
edumatheus65 <ematheusneves13@gmail.com>
2022-04-25 07:53:49 -0300

    EXercise finished

---
+++ separando_digitos_de_um_numero.py
@@ -0,0 +1,7 @@
+num = int(input('Informe um número: '))
+n = str(num)
+print(f"Analisando um número {num}"
+      f"\nUnidade: {(n[3])}"
+      f"\nDezena: {(n[2])}"
+      f"\nCentena: {(n[1])}"
+      f"\nMilhar: {(n[0])}")
